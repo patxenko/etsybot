314047b80298f8cb006399d30f185c6f504c925e
francisco <francisco.lopez@kantar.com>
2023-01-26 12:08:35 +0100

    'cosos'

--- Parser.py
+++ Parser.py
@@ -11,54 +11,17 @@ requests.packages.urllib3.disable_warnings()
 
 class Parser:
     def __init__(self, keyword, country_iso_code):
-        # self.jar = requests.cookies.RequestsCookieJar()
-        # self.session = requests.Session()
-        # self.session.cookies.clear()
-        # response1 = self.session.get('https://www.etsy.com', verify=False, cookies=self.jar)  # or post ...
-        # self.jar.update(response1.cookies)
-        self.cookies = {
-            'uaid': 'oF8m2vz_DE0OOjUPt1mt_R4UzqljZACC5Evmz2F0tVJpYmaKkpVSVlhoflGQq3uWcZGZrlGWb2aKc5BPYn52Rn5isFItAwA.',
-            'user_prefs': 'VxXHLGtl-lsLhgOHomotwhYj2bpjZACC5Evmz2F0tJJraJCSTl5pTo6OUmqebmiwko6SazBUxAhC4SJiGQA.',
-            'fve': '1674721255.0',
-            'last_browse_page': 'https%3A%2F%2Fwww.etsy.com%2F',
-            'ua': '531227642bc86f3b5fd7103a0c0b4fd6',
-            'p': 'eyJnZHByX3RwIjoxLCJnZHByX3AiOjF9',
-            '_gcl_au': '1.1.1316115510.1674723169',
-            '_gid': 'GA1.2.610488509.1674723169',
-            '_pin_unauth': 'dWlkPU1UaGlOVEF6Tm1JdFltRm1NUzAwWkRGakxUZ3lOekV0WW1KaU5ERmpOV1ZsT1RZeQ',
-            'pla_spr': '0',
-            '_ga': 'GA1.1.1533638149.1674723169',
-            '_uetsid': 'cf93db509d5611ed884a4b243ddca5c5',
-            '_uetvid': 'cf94af009d5611ed85cfbd39daa7830e',
-            '_ga_KR3J610VYM': 'GS1.1.1674723169.1.1.1674724016.51.0.0',
-            'tsd': '%7B%22gnav_search_focus%22%3A%7B%22event_name%22%3A%22gnav_search_focus%22%2C%22interaction_type%22%3A%22keyboard%22%7D%7D',
-            'search_options': '{"prev_search_term":"cosas","item_language":null,"language_carousel":null}',
-        }
-        # self.csrf_token = self.get_csrf_token(response1)
+        self.jar = requests.cookies.RequestsCookieJar()
+        self.session = requests.Session()
+        self.session.cookies.clear()
+        response1 = self.session.get('https://www.etsy.com', verify=False, cookies=self.jar)  # or post ...
+        self.jar.update(response1.cookies)
+        self.cookies = self.jar
+
+        self.csrf_token = self.get_csrf_token(response1)
+        self.headers = self.get_headers()
 
 
-        self.headers = {
-            'Accept': '*/*',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            # 'Cookie': 'uaid=oF8m2vz_DE0OOjUPt1mt_R4UzqljZACC5Evmz2F0tVJpYmaKkpVSVlhoflGQq3uWcZGZrlGWb2aKc5BPYn52Rn5isFItAwA.; user_prefs=VxXHLGtl-lsLhgOHomotwhYj2bpjZACC5Evmz2F0tJJraJCSTl5pTo6OUmqebmiwko6SazBUxAhC4SJiGQA.; fve=1674721255.0; last_browse_page=https%3A%2F%2Fwww.etsy.com%2F; ua=531227642bc86f3b5fd7103a0c0b4fd6; p=eyJnZHByX3RwIjoxLCJnZHByX3AiOjF9; _gcl_au=1.1.1316115510.1674723169; _gid=GA1.2.610488509.1674723169; _pin_unauth=dWlkPU1UaGlOVEF6Tm1JdFltRm1NUzAwWkRGakxUZ3lOekV0WW1KaU5ERmpOV1ZsT1RZeQ; pla_spr=0; _ga=GA1.1.1533638149.1674723169; _uetsid=cf93db509d5611ed884a4b243ddca5c5; _uetvid=cf94af009d5611ed85cfbd39daa7830e; _ga_KR3J610VYM=GS1.1.1674723169.1.1.1674724016.51.0.0; tsd=%7B%22gnav_search_focus%22%3A%7B%22event_name%22%3A%22gnav_search_focus%22%2C%22interaction_type%22%3A%22keyboard%22%7D%7D; search_options={"prev_search_term":"cosas","item_language":null,"language_carousel":null}',
-            'Origin': 'https://www.etsy.com',
-            'Referer': 'https://www.etsy.com/search?q=cosas',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-            'X-Page-GUID': 'f3b44e3ed39.1b3933c754eada46dc62.00',
-            'X-Requested-With': 'XMLHttpRequest',
-            'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'x-csrf-token': '3:1674723327:vfQ0ubIeN4B9fe8uS3vXSDtSiR6c:cd27f86c6ff11502c8e5295bbbc137aad2d82ac4800005769a70b6f4e6abfcf5',
-            'x-detected-locale': 'EUR|en-US|ES',
-            'x-recs-primary-location': 'https://www.etsy.com/search?q=cosas',
-            'x-recs-primary-referrer': 'https://www.etsy.com/',
-        }
         # response = self.session.post(
         #     'https://www.etsy.com/api/v3/ajax/bespoke/member/user-preferences/gdpr',
         #     cookies=self.cookies,
@@ -166,11 +129,11 @@ class Parser:
             'specs[async_search_results][1][search_request_params][name_map][results_per_page]': 'result_count',
             'specs[async_search_results][1][search_request_params][name_map][min_price]': 'min',
             'specs[async_search_results][1][search_request_params][name_map][max_price]': 'max',
-            'specs[async_search_results][1][search_request_params][parameters][q]': 'cosas',
-            'specs[async_search_results][1][search_request_params][parameter][page]': self.pagina,
+            'specs[async_search_results][1][search_request_params][parameters][q]': self.keyword,
+            'specs[async_search_results][1][search_request_params][parameter][page]': str(self.pagina),
             'specs[async_search_results][1][search_request_params][parameters][referrer]': 'https://www.etsy.com/es/search?q=' + urllib.parse.quote(
                 self.keyword),
-            'specs[async_search_results][1][search_request_params][parameters][ref]': '',
+            'specs[async_search_results][1][search_request_params][parameters][ref]': 'pagination',
             'specs[async_search_results][1][search_request_params][parameters][is_prefetch]': 'false',
             'specs[async_search_results][1][search_request_params][parameters][placement]': 'wsg',
             'specs[async_search_results][1][search_request_params][user_id]': '',
@@ -184,8 +147,8 @@ class Parser:
             data=data,
             verify=False
         )
-        # self.jar.update(response.cookies)
-        # self.cookies = requests.utils.dict_from_cookiejar(self.jar)
+        self.jar.update(response.cookies)
+        self.cookies = requests.utils.dict_from_cookiejar(self.jar)
         jsondata = response.json()
 
         # OBTENEMOS LOS LAZY LOADED LISTING IDS Y LOS LAZY LOADED AD IDS y lazy_loaded_logging_keys
@@ -267,8 +230,8 @@ class Parser:
             'specs[listingCards][1][search_request_params][name_map][min_price]': 'min',
             'specs[listingCards][1][search_request_params][name_map][max_price]': 'max',
             'specs[listingCards][1][search_request_params][parameters][q]': self.keyword,
-            'specs[listingCards][1][search_request_params][parameters][ref]': 'auto-1',
-            'specs[listingCards][1][search_request_params][parameters][page]': self.pagina,
+            'specs[listingCards][1][search_request_params][parameters][ref]': 'pagination',
+            'specs[listingCards][1][search_request_params][parameters][page]': str(self.pagina),
             'specs[listingCards][1][search_request_params][parameters][referrer]': 'https://www.etsy.com/search?q=' + urllib.parse.quote(
                 self.keyword) + '&ref=auto-1&as_prefix=',
             'specs[listingCards][1][search_request_params][parameters][is_prefetch]': 'false',
@@ -332,8 +295,8 @@ class Parser:
             data=data,
             verify=False
         )
-        # self.jar.update(response.cookies)
-        # self.cookies = requests.utils.dict_from_cookiejar(self.jar)
+        self.jar.update(response.cookies)
+        self.cookies = requests.utils.dict_from_cookiejar(self.jar)
         jsondata = response.json()
         if 'output' in jsondata:
             if 'listingCards' in jsondata['output']:
